16244dcf209d2c730ab8f2865ae6d07a3bcfea16
Jakal02 <gitcontribs@eligrubbs.com>
2024-07-17 17:00:32 -0400

    confirm if card is left or right bower.

--- src/card.rs
+++ src/card.rs
@@ -39,6 +39,23 @@ impl Card {
     pub fn rank(&self) -> Rank {
         self.rank.clone()
     }
+
+    /// returns whether this card is the left bower
+    /// given a trump suit
+    pub fn is_left(&self, trump: Suit) -> bool {
+        self == &Card::new(match trump {
+            Suit::Clubs => Suit::Spades,
+            Suit::Diamonds => Suit::Hearts,
+            Suit::Spades => Suit::Clubs,
+            Suit::Hearts => Suit::Diamonds,
+        }, Rank::Jack)
+    }
+
+    /// returns whether this card is the right bower
+    /// given a trump suit
+    pub fn is_right(&self, trump: Suit) -> bool {
+        self == &Card::new(trump, Rank::Jack)
+    }
 }
 
 #[cfg(test)]
@@ -51,5 +68,21 @@ mod tests {
         assert_eq!(Suit::Hearts, card.suit());
         assert_eq!(Rank::Ten, card.rank());
 
+    }
+
+    #[test]
+    fn is_left() {
+        assert!(Card::new(Suit::Clubs, Rank::Jack).is_left(Suit::Spades));
+        assert!(!Card::new(Suit::Clubs, Rank::Jack).is_left(Suit::Clubs));
+        assert!(!Card::new(Suit::Clubs, Rank::Nine).is_left(Suit::Spades));
+        assert!(!Card::new(Suit::Clubs, Rank::Jack).is_left(Suit::Diamonds));
+    }
+
+    #[test]
+    fn is_right() {
+        assert!(!Card::new(Suit::Clubs, Rank::Jack).is_right(Suit::Spades));
+        assert!(Card::new(Suit::Clubs, Rank::Jack).is_right(Suit::Clubs));
+        assert!(!Card::new(Suit::Clubs, Rank::Nine).is_right(Suit::Spades));
+        assert!(!Card::new(Suit::Diamonds, Rank::Jack).is_right(Suit::Spades));
     }
 }
