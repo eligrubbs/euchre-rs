8e68eb6b2687ea75688c58dbfc653fe5d3653d5c
Jakal02 <gitcontribs@eligrubbs.com>
2024-07-17 13:35:06 -0400

    dealer distributes cards to players.

---
+++ src/dealer.rs
@@ -0,0 +1,42 @@
+use strum::IntoEnumIterator;
+
+use crate::card::{Card, Suit, Rank};
+use crate::player::Player;
+use rand::seq::SliceRandom;
+
+pub struct Dealer {
+    euchre_deck: Vec<Card>,
+}
+
+impl Dealer {
+    pub fn new() -> Dealer {
+        Dealer {
+            euchre_deck: init_euchre_deck(),
+        }
+    }
+
+    pub fn shuffle(&mut self) {
+        self.euchre_deck.shuffle(&mut rand::thread_rng());
+    }
+
+    pub fn deal_n_cards(&mut self, player: &mut Player, num: usize) {
+        let at: usize = self.euchre_deck.len() - num;
+        player.add_cards(&mut self.euchre_deck.split_off(at));
+    }
+
+    pub fn flip_top_card(&mut self) -> Card {
+        self.euchre_deck.pop().unwrap()
+    }
+
+}
+
+
+fn init_euchre_deck() -> Vec<Card> {
+    let mut result:Vec<Card> = vec![];
+    for d_suit in Suit::iter() {
+        for d_rank in Rank::iter() {
+            result.push(Card::new(d_suit, d_rank));
+        }
+    }
+    result
+}
