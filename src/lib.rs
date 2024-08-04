e5eacadd63c85f52721e31dbef8cb5b032268ff4
Jakal02 <gitcontribs@eligrubbs.com>
2024-08-04 09:46:14 -0400

    remove boilerplate lib.rs code.

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,3 @@ pub mod utils;
 pub mod judger;
 pub mod env;
 pub mod agent;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
